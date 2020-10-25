eb90240ba29fd93994efe80201c6cbdbcf5b9519
MaHI <mahimaprakash05@gmail.com>
2020-10-25 19:02:13 +0530

    home page change

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('user/user-home.hbs');
+  let services=[
+    {
+      name:"K P Stores",
+      category:"Retail shop",
+      image:""
+    },
+    {
+      name:"Ayush Clinic",
+      category:"Clinic",
+      image:""
+    }
+  ]
+  res.render('user/user-home.hbs',{ services });
 });
 
 module.exports = router;

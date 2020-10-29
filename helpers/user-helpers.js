c5acb856b3da7dc790f43d948b60eea048a2eeb2
MaHI <mahimaprakash05@gmail.com>
2020-10-29 15:54:01 +0530

    login

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -10,6 +10,30 @@ module.exports={
             })
         })
     },
-    
+    doLogin:(userData)=>{
+        return new Promise(async(resolve,reject)=>{
+            let loginStatus=false
+            let response={}
+            let user=await db.get().collection(collection.USER_COLLECTION).findOne({Email:userData.Email})
+            if(user){
+                bcrypt.compare(userData.Password,user.Password).then((status)=>{
+                    if(status){
+                        console.log('Login Success');
+                        response.user=user
+                        response.status=true
+                        resolve(response)
+                    }
+                    else{
+                        console.log("Login Failed,password mismatch");
+                        resolve({status:false});
+                    }
+                })
+            }else{
+                console.log("Login failed,User not found");
+                resolve({status:false});
+
+            }
+        })
+    }
 
 }
